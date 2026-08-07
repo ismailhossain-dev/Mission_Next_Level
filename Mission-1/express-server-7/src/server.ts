9846e97035340f9013ae85899c4770467868dc45
sabbir <programmarsabbir@gmail.com>
2026-08-08 05:17:07 +0800

    update api create successfully

--- Mission-1/express-server-7/src/server.ts
+++ Mission-1/express-server-7/src/server.ts
@@ -142,6 +142,44 @@ app.get("/api/user/:id", async (req: Request, res: Response) => {
   }
 });
 
+
+// update user 
+app.put("/api/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, password, age } = req.body;
+
+  try {
+    // 1. 'await' add kora hoyeche ebong id=$4 kora hoyeche
+    const result = await pool.query(
+      `UPDATE users SET name = $1, password = $2, age = $3 WHERE id = $4 RETURNING *`, 
+      [name, password, age, id] // Ekhane $4 er jonno id pass kora holo
+    );
+
+    // Jodi user na paoa jay
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found to update!"
+      });
+    }
+
+    // Success response
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0]
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
