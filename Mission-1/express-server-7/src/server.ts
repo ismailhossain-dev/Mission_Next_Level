573e849440ee0a01acf3d7d75fa6e6e6d5908e1b
sabbir <programmarsabbir@gmail.com>
2026-08-08 01:33:26 +0800

    I learned express types and how to learn middlewear server and client

--- Mission-1/express-server-7/src/server.ts
+++ Mission-1/express-server-7/src/server.ts
@@ -1,9 +1,20 @@
-import express from "express";
-const app = express()
-const port = 5000
+import express, { type Application, type Request, type Response } from "express";
+const app:Application = express()
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!')
+app.use(express.json())//eta use korle amra req.body te kono response pabo na
+
+app.get('/', (req: Request, res: Response) => {
+  // res.send('Hello World!!!!!')
+  //Json format data response
+  res.status(200).json({
+    "message": "Express Server",
+    "author": "Sabbir vai"
+  });
+});
+
+app.post("/", async(req:Request, res:Response)=> {
+  console.log(req.body);
 })
 
 app.listen(port, () => {
