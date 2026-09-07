032d3b6d271cd2372d30aac8e8358e7ba3af1394
Mohammad Ismail Hossain <programmarsabbir@gmail.com>
2026-09-08 01:54:24 +0600

    Blog App Registering First User With Prisma And Register API

--- Mission-4/Blog-App/blog-backend/src/app.ts
+++ Mission-4/Blog-App/blog-backend/src/app.ts
@@ -3,20 +3,73 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { prisma } from "./lib/prisma";
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 const app: Application = express();
-app.use(cors({
-  origin:config.app_url,
-  //it will be work in cokkie 
-  credentials:true
-}));
+app.use(
+  cors({
+    origin: config.app_url,
+    //it will be work in cokkie
+    credentials: true,
+  }),
+);
 app.use(express.json());
 //This middleware keeps client data in req.body.
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 //This middleware helps keep data in cokkie
-app.use(cookieParser())
-app.get('/', async(req:Request, res:Response) => {
+app.use(cookieParser());
+app.get("/", async (req: Request, res: Response) => {
   // const user = await prisma.user.findMany();
   // console.log(user)
-  res.send('Hello World!');
+  res.send("Hello World!");
+});
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isUserExist) {
+    throw new Error("User with this email already exists");
+  }
+  //env ta amra jai raki sob gola string hoye jai tai Number e covert korchi
+  const hashPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  //createdUser ta varible rekechi becuase amader user.id ta profile dorkar hobe
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  //after user created then create profile
+  await prisma.profile.create({
+    data: {
+      userId: createdUser.id,
+      profilePhoto,
+    },
+  });
+
+  const user =await prisma.user.findUnique({
+    where: {
+      id: createdUser.id,
+      email: createdUser.email
+    }
+  })
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode : httpStatus.CREATED,
+    message: "User registred successfully",
+    data: user
+  });
 });
 export default app;
