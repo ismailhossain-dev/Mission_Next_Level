8a9d274f57e9ff8267a10de463d289c61a513624
Mohammad Ismail Hossain <programmarsabbir@gmail.com>
2026-09-11 17:55:47 +0600

    Implementing Create Post API And Get All Posts API

--- Mission-4/Blog-App/blog-backend/src/modules/post/post.controller.ts
+++ Mission-4/Blog-App/blog-backend/src/modules/post/post.controller.ts
@@ -3,11 +3,13 @@ import { catechAsync } from "../../utils/catechAsync"
 import { postService } from "./post.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status"
+
+//One user can create multiple post
 const createPost = catechAsync(async(req:Request, res:Response, next:NextFunction)=> {
     const id = req.user?.id;
     const paylaod = req.body; 
 
-    const result = postService.createPost(paylaod, id as string)
+    const result = await postService.createPost(paylaod, id as string)
 
     sendResponse(res, {
         success: true,
@@ -16,7 +18,15 @@ const createPost = catechAsync(async(req:Request, res:Response, next:NextFunctio
         data: result,
     })
 })
-const getAllPosts = catechAsync(async(req:Request, res:Response, next:NextFunction)=> {})  
+const getAllPosts = catechAsync(async(req:Request, res:Response, next:NextFunction)=> {
+    const result = await postService.getAllPosts();
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Get retrived successfully",
+        data: result
+    })
+})  
 const getMyPosts = catechAsync(async (req:Request, res:Response, next:NextFunction)=> {})
 const getPostById = catechAsync((req:Request, res:Response, next:NextFunction)=> {})
 const updatePost = catechAsync(async (req:Request, res:Response, next:NextFunction)=> {})
--- Mission-4/Blog-App/blog-backend/src/modules/post/post.service.ts
+++ Mission-4/Blog-App/blog-backend/src/modules/post/post.service.ts
@@ -14,7 +14,19 @@ const createPost = async(payload:ICreatePostPayload, userId: string) => {
   return result; 
 };
 
-const getAllPosts = () => {};
+const getAllPosts = async() => {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: {
+        omit: {
+          password: true
+        }
+      },
+      comments: true
+    }
+  })
+  return posts; 
+};
 
 const getPostById = () => {};
 
